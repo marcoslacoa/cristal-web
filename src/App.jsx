3807fe9f14a169da8dbf47ea1f0ee81fa9d9fec6
markitos <markitos@pop-os.localdomain>
2026-05-18 05:34:09 -0300

    feat: app completa con todos los componentes integrados

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import QuienesSomos from './components/QuienesSomos'
+import Servicios from './components/Servicios'
+import CorteCNC from './components/CorteCNC'
+import Galeria from './components/Galeria'
+import Contacto from './components/Contacto'
+import Footer from './components/Footer'
+import WhatsappButton from './components/WhatsappButton'
+
 export default function App() {
-  return null
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <QuienesSomos />
+        <Servicios />
+        <CorteCNC />
+        <Galeria />
+        <Contacto />
+      </main>
+      <Footer />
+      <WhatsappButton />
+    </>
+  )
 }
