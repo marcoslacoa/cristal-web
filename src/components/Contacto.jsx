8f766a8b40126b4928603f48198471e074beef4c
markitos <markitos@pop-os.localdomain>
2026-06-04 13:20:07 -0300

    cambio

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -23,7 +23,7 @@ export default function Contacto() {
     setEnviando(true)
     setError('')
     try {
-      const res = await fetch(`${API_URL}/api/contacto-web/`, {
+      const res = await fetch(`${API_URL}/pedidos_vidrio/contacto-web/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
